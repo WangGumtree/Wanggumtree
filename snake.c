e79ebe847a47bb21157210417b93db40043bdde9
baekseojin <seojinbaek100@gmail.com>
2022-12-12 18:07:54 +0900

    REFACTOR :: code explanation

--- snake.c
+++ snake.c
@@ -1,16 +1,16 @@
-#include "main.c"
+#include "main.h"
 
 void checkObject(Node **HEAD, XY *tails, Node **obs) // 오브젝트(벽, 꼬리에 닿였는지 확인)
 {
     Node *tmp = (*HEAD)->tail;
 
-    checkWall(*HEAD); // 벽
+    checkWall(*HEAD); // 벽 닿았는지 확인
 
 
     while (tmp != NULL) // 자기 꼬리
     {
-        if (tmp->x == (*HEAD)->x && tmp->y == (*HEAD)->y)
-            end_Game();
+        if (tmp->x == (*HEAD)->x && tmp->y == (*HEAD)->y) // 만약 자기 꼬리에 머리가 닿이면
+            end_Game(); // 게임 끝
         tmp = tmp->tail;
     }
 
@@ -36,10 +36,10 @@ void moveSnake(Node *HEAD, int s) // 뱀의 이동
     Node *tmp = HEAD;
     XY tmp1 = { HEAD->x,HEAD->y };
     XY tmp2 = tmp1;
-    if (s == SEE_LEFT) // 바라보는 방향으로 이동
-        HEAD->x -= 1;
-    else if (s == SEE_RIGHT)
-        HEAD->x += 1;
+    if (s == SEE_LEFT) // 왼쪽을 바라보고 있으면
+        HEAD->x -= 1; // x좌표 -1(왼쪽으로 가려면 x -1 해야함)
+    else if (s == SEE_RIGHT) // 오른쪽
+        HEAD->x += 1; // x좌표 +1
     else if (s == SEE_UP)
         HEAD->y -= 1;
     else
